40a4d2f3fa4641769845b0b206906d4784326fad
Rakshu-1998 <rakshitha14@gmial.com>
2026-01-14 10:24:14 +0530

    new chaange

--- test.tf
+++ test.tf
@@ -2,7 +2,7 @@ provider "aws"{
   region = "ap-south-1"
 }
 
-resource "aws_ec2_instance" "my_instance" {
+resource "aws_instance" "my_instance" {
   ami           = "ami-0ced6a024bb18ff2e" 
   instance_type = "t2.micro"
   key_name      = "Bunty" # Replace with your key pair name
