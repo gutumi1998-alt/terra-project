c6e48a8d904ddcec3491c9a3d60393c3a35db95d
Rakshu-1998 <rakshitha14@gmial.com>
2026-01-14 12:09:29 +0530

    changing the instance type from t2 to t3

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_instance" {
   ami           = "ami-0ced6a024bb18ff2e" 
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = "Bunty"
 
   tags = {
